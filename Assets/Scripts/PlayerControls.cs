d1a33ea4bb7ebd5008839ed735dafce777190169
KUZIIJ <adpurgin@gmail.com>
2024-05-15 17:19:50 +0200

    ThrowAngles

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] float throwSpeed = 1f;
+    [SerializeField] float throwSpeed = 50f;
+    [SerializeField] float rollMultiplyer = 2f;
+    [SerializeField] float pitchAngle = 30f;
     // Update is called once per frame
     void Update()
     {
@@ -14,30 +16,13 @@ public class PlayerControls : MonoBehaviour
     {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
-        float xOffset = xThrow * Time.deltaTime*throwSpeed;
-        float yOffset = yThrow * Time.deltaTime*throwSpeed;
-        float newXPos = transform.localPosition.z + xOffset;
-        float newYPos = transform.localPosition.y + yOffset;
-        if (newXPos >= 20f)
-        {
-            newXPos = 20f;
-        }
-        else if (newXPos <= -20f)
-        {
-            newXPos = -20f;
-        }
-        if (newYPos >= 19f)
-        {
-            newYPos = 19f;
-        }
-        else if (newYPos <= -3f)
-        {
-            newYPos = -3f;
-        }
+        float xOffset = xThrow * Time.deltaTime * throwSpeed;
+        float yOffset = yThrow * Time.deltaTime * throwSpeed;
+        float yAngleOffset = yThrow * Time.deltaTime;
+        float newXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -15f, 15f);
+        float newYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -2f, 14f);
 
-
-        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, newXPos); // Axis Z calld as X because of some bug in unity Z and X are mixed out. IN script X is Z and Z is X.
-
-
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        transform.localRotation = Quaternion.Euler(yAngleOffset * -pitchAngle, 0, newXPos * rollMultiplyer);
     }
 }
