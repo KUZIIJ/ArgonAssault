cb2deed59a56ac6609302bd527fae3da5795440c
KUZIIJ <adpurgin@gmail.com>
2024-05-15 14:09:51 +0200

    PlayerMovement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -14,8 +14,30 @@ public class PlayerControls : MonoBehaviour
     {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
-        //transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z+0.2f*throwSpeed*Time.deltaTime);
+        float xOffset = xThrow * Time.deltaTime*throwSpeed;
+        float yOffset = yThrow * Time.deltaTime*throwSpeed;
+        float newXPos = transform.localPosition.z + xOffset;
+        float newYPos = transform.localPosition.y + yOffset;
+        if (newXPos >= 20f)
+        {
+            newXPos = 20f;
+        }
+        else if (newXPos <= -20f)
+        {
+            newXPos = -20f;
+        }
+        if (newYPos >= 19f)
+        {
+            newYPos = 19f;
+        }
+        else if (newYPos <= -3f)
+        {
+            newYPos = -3f;
+        }
+
+
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, newXPos); // Axis Z calld as X because of some bug in unity Z and X are mixed out. IN script X is Z and Z is X.
+
 
     }
 }
